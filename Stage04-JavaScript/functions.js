2b3eab20de1126fbf01cc5414c376db90698f6fb
Matheus <mat.araujoribeiro@gmail.com>
2023-11-23 14:52:39 -0300

    [add] functions

--- Stage04-JavaScript/functions.js
+++ Stage04-JavaScript/functions.js
@@ -1,9 +1,24 @@
 // Frases motivacionais
-
+let randInt1 = Math.floor(Math.random() * 101)
+let randInt2 = Math.floor(Math.random() * 101)
 function mot(){
   let frases = ['você consegue', 'continue perseverando', 'quem acredita sempre alcança']
-  let randInt = Math.floor(Math.random() * 3)
-  console.log(frases[randInt]); 
+  let count = 1
+  while(count <= frases.length)
+    { 
+      let randInt = Math.floor(Math.random() * 3)
+      console.log(frases[randInt]);
+      count++
+    }
 }
-
 mot()
+
+function sum(a, b){
+  return a+b
+}
+console.log(sum(randInt1, randInt2));
+
+let subject = 'study'
+function create(subject){
+
+}
