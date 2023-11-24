6b0bc8140eb6b5720c3b73e8d08d37679e43e939
Matheus <mat.araujoribeiro@gmail.com>
2023-11-24 11:08:55 -0300

    [add] guessing game

--- stage04-algoritmos/algoritmos.js
+++ stage04-algoritmos/algoritmos.js
@@ -35,4 +35,30 @@
 
 
 // 5. Jogo de adivinhação
-let numSorteado = Number(Math.random()*10)
+var lin = ()=>{
+    console.log("---------");
+}
+
+let numSorteado = Number(Math.round(Math.random()*3))
+let userGuess = Number(prompt("Adivinhe o número que estou pensando de 1 a 3"))
+let tentativas = 0
+
+if (numSorteado == userGuess){
+    console.log("PARABÉNS! VC ACERTOU DE PRIMEIRA");
+    alert("VC ACERTOU DE PRIMEIRA")
+}
+
+while(numSorteado != userGuess){
+    lin()
+    console.log(`Sorteado da rodada: ${numSorteado}`);
+    userGuess = Number(prompt("Errou! Tente novamente: "))
+    tentativas++
+    if (numSorteado == userGuess){
+        tentativas++
+        lin()
+        console.log(`PARABÉNS! Nº sorteado: ${numSorteado}`);
+        console.log(`Seu Palpite: ${userGuess}`);
+        console.log(`Nº de tentativas: ${tentativas}`);
+        break;
+    }
+}
