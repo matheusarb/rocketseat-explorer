0d0a2948e00c3bf58eab9f249fd6a2b7ee1d0114
Matheus <mat.araujoribeiro@gmail.com>
2023-11-24 10:52:02 -0300

    [add] algos

---
+++ stage04-algoritmos/algoritmos.js
@@ -0,0 +1,38 @@
+// 1. perguntar nome e mostrar
+// var nome = prompt('Qual seu nome')
+// console.log("Olá, seu m"+nome);
+
+
+// 2. somar dois nºs
+// .|.
+// var n1 = Number(prompt('digite um nº: ')) 
+// var n2 = Number(prompt('digite um nº: ')) 
+// console.log(n1+n2);
+
+
+// 3. meédia de estudante
+// var media, n1, n2, n3;
+// n1 = Number(prompt('Digite sua 1ª nota'))
+// n2 = Number(prompt('Digite sua 2ª nota'))
+// n3 = Number(prompt('Digite sua 3ª nota'))
+// media = (n1+n2+n3)/3
+// console.log(media);
+
+
+// 4. lista de compras
+// var lista = new Array()
+// for(let i = 0; i < 3; i++){
+    // item = String(prompt('Que item deseja adicionar? '))
+    // lista.push(item)
+// }
+// console.log(lista);
+
+
+// let items = []
+// for(let i = 0; i < 2; i++){
+//     let item = prompt("Digite item "+(i+1))
+// }
+
+
+// 5. Jogo de adivinhação
+let numSorteado = Number(Math.random()*10)
