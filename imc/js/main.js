1d798ec6f0f73c1bd110d2b73c956489bf27a000
Matheus <mat.araujoribeiro@gmail.com>
2023-11-27 19:02:11 -0300

    [add] clean code

--- imc/js/main.js
+++ imc/js/main.js
@@ -1,40 +1,31 @@
 import { Modal } from "./modal.js";
 import { AlertError } from "./alert-error.js";
+import { notANumber, imcCalc } from "./utils.js";
 
 const form = document.querySelector("form");
 const inputWeight = document.querySelector("#weight");
 const inputHeight = document.querySelector("#height");
 
-
-/* Formas de submeter o form
-1. att onsubmit na tag <form>, passando uma função
-*/
-
 form.onsubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     
-    const weightValue = inputWeight.value
-    const heightValue = inputHeight.value
-    const showAlertError = notANumber(weightValue) || notANumber(heightValue)
+    const weightValue = inputWeight.value;
+    const heightValue = inputHeight.value;
+    const weightOrHeightIsNotANumber = notANumber(weightValue) || notANumber(heightValue);
     
-    if (showAlertError) {
+    if (weightOrHeightIsNotANumber) {
         AlertError.open();
         return;
     }
-    AlertError.close()
-
-    const imc = imcCalc(weightValue, heightValue)
-    Modal.msgImc.innerHTML = `Seu IMC é de ${imc}`
-    Modal.open();
+    AlertError.close();
+    
+    const imc = imcCalc(weightValue, heightValue);
+    displayImcMsg(imc);
 }
 
-Modal.btnClose.onclick = () => Modal.close()
+Modal.btnClose.onclick = () => Modal.close();
 
-function notANumber(value) {
-    return isNaN(value) || value == ""
+function displayImcMsg(imc) {
+    Modal.msgImc.innerHTML = `Seu IMC é de ${imc}`;
+    Modal.open()
 }
-
-function imcCalc(weight, height) {
-    return (weight / ((height/100) ** 2 )).toFixed(2)
-}
-
--- imc/js/utils.js
+++ imc/js/utils.js
@@ -1,11 +1,7 @@
-function test1() {
-    console.log("test1");
+export function notANumber(value) {
+    return isNaN(value) || value == ""
 }
-function test2() {
-    console.log("teste2");
+
+export function imcCalc(weight, height) {
+    return (weight / ((height/100) ** 2 )).toFixed(2)
 }
-// só pode usar um default por arquivo
-export default test1
-// named export (exportar mais de 1 através de lista)
-export {test1, test2}
-export const today = () => new Date();
